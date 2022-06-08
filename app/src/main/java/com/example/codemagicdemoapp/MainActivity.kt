3aa509ab17aa572bd456cc989b052542eb6d0db8
santanu901 <santanumukherjee901@gmail.com>
2022-06-08 15:02:01 +0530

    databinding added

--- app/src/main/java/com/example/codemagicdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/codemagicdemoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.codemagicdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.codemagicdemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initValue()
+    }
+
+    private fun initValue() {
+        binding.tvLabel.text = "Static Polymorphism: ${add(1, 2)}"
+    }
+
+    private fun add(arg1: Int, arg2: Int): Int {
+        return (arg1 + arg2)
+    }
+
+    private fun add(arg1: Int, name: String): String {
+        return "$name-$arg1"
+    }
+
+    private fun add(arg1: Int, arg2: Int, arg3: Int): Int {
+        return (arg1 + arg2 + arg3)
     }
 }
